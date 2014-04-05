e83f1d7492bc41387d908bc8d5c28f89da117178
GarryLaly <garrylaly@gmail.com>
2014-04-05 08:49:02 +0700

    My Commit 05 April 2014

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
-
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end("Hello World!");
-});
-server.listen(1000);
+	 
+	var server = http.createServer(function(req, res) {
+	  res.writeHead(200);
+	  res.end('Contoh di browser');
+	});
+	server.listen(1000);
