bbe1883ccd62c95f6eb8022b421b7038afae51e5
GarryLaly <garrylaly@gmail.com>
2014-04-05 08:54:50 +0700

    My Commit 05 April 2014

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 	 
-	var server = http.createServer(function(req, res) {
-	  res.writeHead(200);
-	  res.end('Contoh di browser');
-	});
-	server.listen(1000);
+	http.createServer(function (req, res) {
+	 res.writeHead(200, {'Content-Type': 'text/plain'});
+	 res.end('Hello World\n');
+	}).listen(8090, '127.0.0.1');
+	console.log('Server running at http://127.0.0.1:8090/');
